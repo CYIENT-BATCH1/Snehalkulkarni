8ba486c77d233ad80a4f9c5fe1e9c4c938deec8c
Snehal-69029 <108972540+Snehal-69029@users.noreply.github.com>
2022-07-09 13:31:22 +0530

    Create day_1problems_1.c

---
+++ day_1problems_1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    int n;
+    printf("enter the number\n");
+    scanf("%d",&n);
+    if(n%11==0)
+    printf("special");
+    else
+    printf("not special");
+    return 0;
+}
